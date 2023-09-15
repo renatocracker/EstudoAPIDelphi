049d8e33aae5da8fe2b1ae9cfeca805f8597041d
Renato Alves Costa <renatocracker@hotmail.com>
2023-09-15 00:05:48 -0300

    Commit 20230915
    Criando corpo da api e testes iniciais com o postman

--- EstudoAPIDelphi.dpr
+++ EstudoAPIDelphi.dpr
@@ -4,22 +4,67 @@ program EstudoAPIDelphi;
 
 {$R *.res}
 
-uses Horse;
+uses
+  Horse, Horse.Compression, Horse.BasicAuthentication, Horse.Jhonson, Horse.Commons, System.SysUtils, System.JSON;
 
 var
-  App : THorse;
+  Users : TJSONArray;
 
-  begin
-    App := THorse.Create(9000);
+begin
+  THorse.Use(Compression()); // CHAMAR ANTES DO JHONSON
+  THorse.Use(Jhonson());
 
-    App.Get('/ping',
-      procedure(Req: THorseRequest; Res: THorseResponse; Next: TProc)
-      begin
-        Res.Send('pong');
-      end);
+  // AUTENTICAÇÃO
+  THorse.Use(HorseBasicAuthentication(
+    function(const AUsername, APassword: string): Boolean
+    begin
+      Result := AUsername.Equals('renato') and APassword.Equals('123');
+    end));
 
-    App.Start;
-    --THorse.Listen(9000);
-  end.
+    // TESTE DE COMPRESSÃO
+  THorse.Get('/ping',
+    procedure(Req: THorseRequest; Res: THorseResponse; Next: TProc)
+    var
+      I: Integer;
+      LPong: TJSONArray;
+    begin
+      LPong := TJSONArray.Create;
+      for I := 0 to 1000 do
+        LPong.Add(TJSONObject.Create(TJSONPair.Create('ping', 'pong')));
+      Res.Send(LPong);
+    end);
 
+  Users := TJSONArray.Create;
+
+  // LISTAR
+  THorse.Get('/users',
+    procedure(Req: THorseRequest; Res: THorseResponse; Next: TProc)
+    begin
+      Res.Send<TJSONAncestor>(Users.Clone);
+    end);
+
+  // INSERIR
+  THorse.Post('/users',
+    procedure(Req: THorseRequest; Res: THorseResponse; Next: TProc)
+    var
+      User : TJSONObject;
+    begin
+      User := Req.Body<TJSONObject>.Clone as TJSONObject;
+      Users.AddElement(User);
+      Res.Send<TJSONAncestor>(User.Clone).Status(THTTPStatus.Created);
+    end);
+
+  // EXCLUIR
+  THorse.Delete('/users/:id',
+    procedure(Req: THorseRequest; Res: THorseResponse; Next: TProc)
+    var
+      Id : Integer;
+    begin
+      id := Req.Params.Items['id'] .ToInteger;
+      Users.Remove(Pred(id)).Free;
+      Res.Send<TJSONAncestor>(Users.Clone).Status(THTTPStatus.Created);
+    end);
+
+  THorse.Listen(9000);
 end.
+
